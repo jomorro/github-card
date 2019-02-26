288f7a8badc208fed3bac2d76acac7c340a598bc
jomorro <morrowjoe0@gmail.com>
2019-02-26 12:34:30 -0500

    Updates

---
+++ precache-manifest.4581a6d1f6b57ad6d31ae4d0d5a8566b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5813c926338b9347bf0",
+    "url": "/github-card/static/js/runtime~main.e5813c92.js"
+  },
+  {
+    "revision": "444254bdb5afe09448eb",
+    "url": "/github-card/static/js/main.444254bd.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/github-card/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "444254bdb5afe09448eb",
+    "url": "/github-card/static/css/main.ba028512.chunk.css"
+  },
+  {
+    "revision": "5c87d1d78208fc1afee6d08d1fa557fc",
+    "url": "/github-card/index.html"
+  }
+];
